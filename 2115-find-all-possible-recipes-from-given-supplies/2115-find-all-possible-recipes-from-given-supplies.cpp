ec85e8ec9ad478d275d322e03fa671e11125c91d
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2025-03-21 16:36:46 +0530

    Time: 48 ms (85.46%), Space: 100.9 MB (89.52%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string, int> recipeIdx, sset;
+        unordered_map<string, int> recipeIdx, cookable;
         function<bool(string&)> dfs = [&](string& rec) -> bool{
-            if(sset.count(rec))
-                return sset[rec];
+            if(cookable.count(rec))
+                return cookable[rec];
             if(recipeIdx.count(rec) == 0)
                 return false;
-            sset[rec] = false;
+            cookable[rec] = false;
             for(auto& it: ingredients[recipeIdx[rec]])
                 if(!dfs(it))
                     return false;
-            return sset[rec] = true;
+            return cookable[rec] = true;
         };
         vector<string> ans;
         for(int i = 0; i < recipes.size(); i++)
             recipeIdx[recipes[i]] = i;
         for(auto ing: supplies)
-            sset[ing] = true;
+            cookable[ing] = true;
         for(auto& recipe: recipes)
             if(dfs(recipe))
                 ans.push_back(recipe);
