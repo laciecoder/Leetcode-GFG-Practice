cb3570f6af12754a603cc2b116d8600a9018e139
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-01-06 22:59:32 +0530

    Time: 4 ms (0.56%), Space: 11.6 MB (15.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        digits.back() += 1;
-        if(digits.back() / 10){
-            int rem = 0;
-            for(int i = digits.size() - 1; i >= 0; i--){
-                digits[i] += rem;
-                rem = digits[i] / 10;
-                digits[i] %= 10;
-                if(!rem)
-                    break;
-            }
-            if(rem)
-                digits.insert(digits.begin(), rem);
+        
+        for(int i = digits.size() - 1; i >= 0; i--){
+            digits[i]++;
+            if(digits[i] < 10)
+                return digits;
+            digits[i] = 0;
         }
+
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
