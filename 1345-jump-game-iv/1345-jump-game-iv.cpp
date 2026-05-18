f3c684fdae6b8cd612ec5f0763c76c1be1285cb0
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-05-18 22:42:32 +0530

    Time: 80 ms (69.39%), Space: 75.3 MB (82.13%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -4,11 +4,11 @@ public:
         int PTR = INT_MIN;
         unordered_map<int, vector<int>> mp;
         int n = arr.size();
-        unordered_set<int> visited;
+        vector<bool> visited(n);
         queue<int> q;
         q.push(0);
         q.push(PTR);
-        visited.insert(0);
+        visited[0] = true;
         for(int i = 0; i < n; i++){
             mp[arr[i]].push_back(i);
         }
@@ -24,17 +24,17 @@ public:
             }
             if(front == n - 1)
                 return time;
-            if(front - 1 >= 0 && !visited.count(front - 1)){
+            if(front - 1 >= 0 && !visited[front - 1]){
                 q.push(front - 1);
-                visited.insert(front - 1);
+                visited[front - 1] = true;
             }
-            if(front + 1 < n && !visited.count(front + 1)){
+            if(front + 1 < n && !visited[front + 1]){
                 q.push(front + 1);
-                visited.insert(front + 1);
+                visited[front + 1] = true;
             }
             for(int next: mp[arr[front]]){
-                if(!visited.count(next)){
-                    visited.insert(next);
+                if(!visited[next]){
+                    visited[next] = true;
                     q.push(next);
                 }
             }
