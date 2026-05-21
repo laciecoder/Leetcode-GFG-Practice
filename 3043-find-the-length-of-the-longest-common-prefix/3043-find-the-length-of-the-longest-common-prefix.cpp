39c7f36a8bb77b41211f293aac88a8bb62ee5cd2
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-05-21 21:25:04 +0530

    Time: 143 ms (69.5%), Space: 134.6 MB (95.5%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    class Trie{
-        public:
-        vector<Trie*> nums;
-        Trie() {
-            nums = vector<Trie*>(10);
-        }
-    };
-    void insert(Trie* root, string str){
-        for(char ch: str){
-            int pos = ch - '0';
-            if(!root -> nums[pos]){
-                root -> nums[pos] = new Trie();
-            }
-            root = root -> nums[pos];
-        }
-    }
-
-    int depth(Trie* root, string str){
-        int count = 0;
-        for(char ch: str){
-            int pos = ch - '0';
-            if(!root -> nums[pos])
-                return count;
-            count++;
-            root = root -> nums[pos];            
-        }
-        return count;
-    }
-
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        Trie* root = new Trie();
-        for(int num: arr2){
-            insert(root, to_string(num));
+        unordered_set<int> set;
+        for(int v: arr2){
+            while(v){
+                set.insert(v);
+                v /= 10;
+            }
         }
         int ans = 0;
-        for(int num: arr1){
-            ans = max(ans, depth(root, to_string(num)));
+
+        for(int v: arr1){
+            while(v > 0 && set.count(v) == 0){
+                v /= 10;
+            }
+            int cnt = 0;
+            while(v > 0){
+                cnt++;
+                v /= 10;
+            }
+            ans = max(ans, cnt);
         }
+
         return ans;
     }
 };
