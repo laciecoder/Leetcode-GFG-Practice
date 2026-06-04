7261b272d2d4667bcc6bbabe50988a921723ca8e
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-06-04 22:48:21 +0530

    Time: 1199 ms (5.04%), Space: 305.4 MB (6.34%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int getWaveCount(string& str){
-        int n = str.size();
-        int wave = 0;
-        for(int i = 1; i < n - 1; i++){
-            wave += ((str[i - 1] < str[i] && str[i] > str[i + 1]) || (str[i - 1] > str[i] && str[i] < str[i + 1]));
+    vector<long long> dp, pref;
+    Solution(){
+        long long limit = 1e5+1;
+        dp = vector<long long>(limit);
+        pref = vector<long long>(limit);
+        for(long long i = 100; i < limit; i++){
+            int right = i % 10;
+            int mid = (i / 10) % 10;
+            int left = (i / 100) % 10;
+            dp[i] = dp[i / 10] + ((mid > right && mid > left) | (mid < right && mid < left));
+            pref[i] = pref[i - 1] + dp[i];
         }
-        return wave;
     }
-    int totalWaviness(int num1, int num2) {
-        if(num2 <= 100)
-            return 0;
-        int cnt = 0;
-
-        for(int i = num1; i <= num2; i++){
-            string str = to_string(i);
-            cnt += getWaveCount(str);
-        }
-
-
-        return cnt;
+    long long totalWaviness(long long num1, long long num2) {
+        return pref[num2] - pref[num1 - 1];
     }
 };
