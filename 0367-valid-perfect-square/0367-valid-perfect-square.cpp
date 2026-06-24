8192f13410afd9e27842458f1e92e50d48816769
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-06-24 22:34:12 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.55%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPerfectSquare(unsigned long long num) {
         unsigned long long start = 1, end = num;
-        unsigned long long ans = start;
         while(start <= end){
             unsigned long long mid = start + (end - start) / 2;
             if(mid * mid == num)
@@ -11,10 +10,9 @@ public:
                 end = mid - 1;
             }
             else{
-                ans = start;
                 start = mid + 1;
             }
         }
-        return ans * ans == num;
+        return false;
     }
 };
