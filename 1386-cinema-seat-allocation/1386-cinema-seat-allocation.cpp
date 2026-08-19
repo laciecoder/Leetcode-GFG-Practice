31253e285178ee386203a3cf57ef23709a37c81a
Ramanesh P V <67573747+laciecoder@users.noreply.github.com>
2026-08-19 22:56:48 +0530

    Time: 47 ms (50.68%), Space: 66.9 MB (46.96%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         long long ans = n * 2;
-        unordered_map<int, vector<int>> mp;
+        unordered_map<int, int> mp;
         for(auto rs: reservedSeats){
             int row = rs[0], col = rs[1];
-            if(!mp.count(row))
-                mp[row] = vector<int>(11,1);
-            mp[row][col] = -1;
+            
+            mp[row] |= (1 << col);
         }
+        int twofivemask = 0b0000111100;
+        int foursevenmask = 0b0011110000;
+        int sixninemask = 0b1111000000;
         for(auto [_, row]: mp){
             ans -= 2;
-            int twofive = row[2] + row[3] + row[4] + row[5];
-            int fourseven = row[4] + row[5] + row[6] + row[7];
-            int sixnine = row[6] + row[7] + row[8] + row[9];
-            if(twofive == 4 && sixnine == 4){
+            
+            if((row & twofivemask) == 0 && (row & sixninemask) == 0){
                 ans += 2;
                 continue;
             }
-            ans = ans + ((twofive == 4) or (fourseven == 4) or (sixnine == 4));
+            ans = ans + (((row & twofivemask) == 0) or ((row & foursevenmask) == 0) or ((row & sixninemask) == 0));
         }
         return ans;
     }
